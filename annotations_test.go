e3d3e88aa99b83479b18af8802b7dfb175977b86
Liam Stanley <liam@liam.sh>
2024-06-23 15:14:22 -0400

    fix: remove tests that improve coverage but don't really test much
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- annotations_test.go
+++ annotations_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	"entgo.io/ent/entc/gen"
-	"github.com/ogen-go/ogen"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -139,99 +138,6 @@ func TestAnnotation_Merge(t *testing.T) {
 				out, _ = out.Merge(a).(Annotation)
 			}
 			assert.Equal(t, tt.want, out)
-		})
-	}
-}
-
-func TestAnnotation_With(t *testing.T) {
-	tests := []struct {
-		name       string
-		annotation Annotation
-		want       Annotation
-	}{
-		{name: "no-annotation", annotation: Annotation{}, want: Annotation{}},
-		{
-			name:       "with-operation-summary",
-			annotation: WithOperationSummary(OperationCreate, "foo"),
-			want: Annotation{
-				OperationSummary: map[Operation]string{
-					OperationCreate: "foo",
-				},
-			},
-		},
-		{
-			name:       "with-operation-description",
-			annotation: WithOperationDescription(OperationCreate, "foo"),
-			want: Annotation{
-				OperationDescription: map[Operation]string{
-					OperationCreate: "foo",
-				},
-			},
-		},
-		{
-			name:       "with-additional-tags",
-			annotation: WithAdditionalTags("foo", "bar"),
-			want:       Annotation{AdditionalTags: []string{"foo", "bar"}},
-		},
-		{
-			name:       "with-tags",
-			annotation: WithTags("foo", "bar"),
-			want:       Annotation{Tags: []string{"foo", "bar"}},
-		},
-		{
-			name:       "with-operation-id",
-			annotation: WithOperationID(OperationCreate, "foo"),
-			want: Annotation{
-				OperationID: map[Operation]string{
-					OperationCreate: "foo",
-				},
-			},
-		},
-		{name: "with-description", annotation: WithDescription("foo"), want: Annotation{Description: "foo"}},
-		{name: "with-pagination", annotation: WithPagination(true), want: Annotation{Pagination: ptr(true)}},
-		{name: "with-min-items-per-page", annotation: WithMinItemsPerPage(10), want: Annotation{MinItemsPerPage: 10}},
-		{name: "with-max-items-per-page", annotation: WithMaxItemsPerPage(10), want: Annotation{MaxItemsPerPage: 10}},
-		{name: "with-items-per-page", annotation: WithItemsPerPage(10), want: Annotation{ItemsPerPage: 10}},
-		{name: "with-eager-load", annotation: WithEagerLoad(true), want: Annotation{EagerLoad: ptr(true)}},
-		{name: "with-edge-endpoint", annotation: WithEdgeEndpoint(true), want: Annotation{EdgeEndpoint: ptr(true)}},
-		{
-			name:       "with-filter",
-			annotation: WithFilter(FilterGroupArray | FilterGroupLength),
-			want:       Annotation{Filter: FilterGroupArray | FilterGroupLength},
-		},
-		{name: "with-handler", annotation: WithHandler(true), want: Annotation{Handler: ptr(true)}},
-		{name: "with-sortable", annotation: WithSortable(true), want: Annotation{Sortable: true}},
-		{name: "with-skip", annotation: WithSkip(true), want: Annotation{Skip: true}},
-		{name: "with-read-only", annotation: WithReadOnly(true), want: Annotation{ReadOnly: true}},
-		{
-			name:       "with-example",
-			annotation: WithExample(map[string]any{"foo": "bar"}),
-			want:       Annotation{Example: map[string]any{"foo": "bar"}},
-		},
-		{
-			name:       "with-schema",
-			annotation: WithSchema(&ogen.Schema{Ref: "#/components/schemas/foo"}),
-			want:       Annotation{Schema: &ogen.Schema{Ref: "#/components/schemas/foo"}},
-		},
-		{
-			name:       "with-include-operations",
-			annotation: WithIncludeOperations(OperationCreate, OperationRead),
-			want:       Annotation{Operations: []Operation{OperationCreate, OperationRead}},
-		},
-		{
-			name:       "with-exclude-operations",
-			annotation: WithExcludeOperations(OperationCreate, OperationRead),
-			want: Annotation{Operations: []Operation{
-				OperationUpdate,
-				OperationDelete,
-				OperationList,
-			}},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.want, tt.annotation)
 		})
 	}
 }
