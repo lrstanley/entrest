6d6b9fca6feca4b271c98fef6650068df74a19b2
Liam Stanley <liam@liam.sh>
2024-07-13 15:03:30 -0400

    add WIP notice just in case
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- docs/content/config.ts
+++ docs/content/config.ts
@@ -1,6 +1,15 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    schema: docsSchema({
+      extend: z.object({
+        // global banner thanks to: https://hideoo.dev/notes/starlight-sitewide-banner
+        banner: z.object({ content: z.string() }).default({
+          content: `Documentation &amp; entrest itself are a work in progress (expect breaking changes). Check out the <a href="https://github.com/lrstanley/entrest" target="_blank">GitHub Project</a> to contribute.`,
+        }),
+      }),
+    }),
+  }),
 };
